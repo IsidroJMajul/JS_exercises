be8e06f84eb7bd50e8163fc0696840a77747e956
Art_Boucaya <30676835+IsidroJMajul@users.noreply.github.com>
2024-07-24 01:39:10 +0000

    01-variables

---
+++ 01-variables.js
@@ -0,0 +1,27 @@
+// var
+// NO RECOMENDABLE
+// Se pueden reasignar. Se pueden volver a cambiar.
+
+var helloWorld = "Hello Javascript, from Isidro"
+console.log(helloWorld)
+
+helloWorld = "Chau Javascript"
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "Hello Javascript, from Isidro...de nuevo"
+console.log(helloWorld2)
+
+helloWorld2 = "Chau otra vez, Javascript"
+console.log(helloWorld2)
+
+// const
+// NO se pueden reasignar.
+
+const helloWorld3 = "Hooooola JS"
+console.log(helloWorld3)
+
+/* Error
+helloWorld3 = "Chau, chau?"
+console.log(helloWorld3) */
