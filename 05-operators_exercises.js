0a5cb56926f285c6ea03557d3d5c3dde50f8cc05
Art_Boucaya <30676835+IsidroJMajul@users.noreply.github.com>
2024-07-27 11:38:34 +0000

    -

--- 05-operators_exercises.js
+++ 05-operators_exercises.js
@@ -6,10 +6,35 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Crea una variable para cada operación aritmética
 
+let nro1 = 5
+let nro2 = 9
+
+function suma(){
+    return nro1 + nro2
+};
+
+let resta = nro1 - nro2
+let multiplicacion = nro1 * nro2
+let division = nro1 / nro2
+let porcentaje = nro1 % nro2
+let exponencial = nro1 ** nro2
+
+console.log (nro1)
+
+
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
+let myNewVariable = resta
+myNewVariable += 3
+
+console.log(myNewVariable)
+
 // 3. Imprime 5 comparaciones verdades con diferentes operadores de comparación
+
+let myVariable = -1
+console.log(myVariable == myNewVariable)
+console.log(myVariable > myNewVariable)
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
 
@@ -22,5 +47,9 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 // 8. Añade alguna negación
 
 // 9. Utiliza el operador ternario
+
+const iAmDeveloper = true
+
+iAmDeveloper ? console.log("tienes que seguir practicando") : console.log("dedícate a otra cosa")
 
 // 10. Combina operadores aritméticos, de comparáción y lógicas
--- 07-strings_exercises.js
+++ 07-strings_exercises.js
@@ -6,20 +6,51 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Concatena dos cadenas de texto
 
+let myPc = "EliteBook"
+
+console.log("Hola, esta es tu pc: " + myPc)
+
 // 2. Muestra la longitud de una cadena de texto
+
+console.log(myPc.length)
 
 // 3. Muestra el primer y último carácter de un string
 
+console.log(myPc[0])
+console.log(myPc[8])
+
 // 4. Convierte a mayúsculas y minúsculas un string
+
+console.log(myPc.toLowerCase())
+console.log(myPc.toUpperCase())
 
 // 5. Crea una cadena de texto en varias líneas
 
+console.log(`esta es
+    mi cadena de texto
+    de varias lineas`)
+
 // 6. Interpola el valor de una variable en un string
+
+let myHome = `Mi vieja ${myPc} necesita un cambio`
+console.log(myHome)
 
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
 
+let myHomeConGuiones = myHome.replaceAll(" ", "-")
+console.log(myHomeConGuiones)
+
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+console.log(myHome.includes("cambio"))
 
 // 9. Comprueba si dos strings son iguales
 
+console.log(myHome == myHomeConGuiones)
+
 // 10. Comprueba si dos strings tienen la misma longitud
+
+console.log(myHome.length)
+console.log(myHomeConGuiones.length)
+
+console.log(myHome.length == myHomeConGuiones.length)
