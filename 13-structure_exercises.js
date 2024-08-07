b9f1d0b2135f47bcfc70890c0a9cad1f4711832b
Art_Boucaya <30676835+IsidroJMajul@users.noreply.github.com>
2024-08-07 20:29:07 +0000

    -

--- 13-structure_exercises.js
+++ 13-structure_exercises.js
@@ -6,13 +6,37 @@ https://www.twitch.tv/videos/2212289583?t=00h17m45s
 
 // 1. Crea un array que almacene cinco animales
 
+let arrayAnimals = []
+
+arrayAnimals = ["león", "delfín", "tortuga", "tigre", "elefante"]
+
 // 2. Añade dos más. Uno al principio y otro al final
+
+arrayAnimals.push("yacaré")
+
+arrayAnimals.unshift("serpiente")
+
+console.log(arrayAnimals)
 
 // 3. Elimina el que se encuentra en tercera posición
 
+delete arrayAnimals[3]
+console.log(arrayAnimals)
+
 // 4. Crea un set que almacene cinco libros
 
+let mySet = new Set()
+
+mySet = new Set(["Harry Potter", "Odiseo", "Rayuela", "1984", "Ficciones"])
+
+console.log(mySet)
+
 // 5. Añade dos más. Uno de ellos repetido
+
+mySet.add("Cosmos")
+mySet.add("Harry Potter")
+
+console.log(mySet)
 
 // 6. Elimina uno concreto a tu elección
 
