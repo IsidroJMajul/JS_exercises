5252aad41736b6129dd476d56ab13b9d00fc7f17
Art_Boucaya <30676835+IsidroJMajul@users.noreply.github.com>
2024-08-08 10:06:54 +0000

    -

---
+++ 18-objects.js
@@ -0,0 +1,92 @@
+// OBJETOS
+// Colección de propiedades. Similar a maps.
+
+// Sintaxis
+
+let person = {
+    name: "Isidro",
+    age: 32,
+    alias: "Art_Boucaya"
+}
+
+// Acceso a propiedades
+
+// Notación punto
+console.log(person.name)
+
+// Notación de corchetes
+console.log(person["age"])
+
+// Modificación
+// tamb se puede cambiar el tipo de dato
+person.name = "Isidro Juan Majul"
+person.age = "32"
+
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person.age)
+
+// Eliminar props
+
+delete person.age
+console.log(person)
+
+// Añadir props
+
+person.email = "isidro@gmail.com"
+console.log(person)
+
+// Métodos (funciones)
+
+let person2 = {
+    name: "Isidro",
+    age: 32,
+    alias: "Art_Boucaya",
+    walk: function () { // agrego función
+        console.log("la persona camina")
+    }
+}
+
+person2.walk()
+
+// Anidar objetos
+
+let person3 = {
+    name: "Isidro",
+    age: 32,
+    alias: "Art_Boucaya",
+    walk: function () { // agrego función
+        console.log("la persona camina")
+    },
+    job: { // añado objeto
+        name: "Programador",
+        exp: 7
+    } 
+}
+
+console.log(person3)
+
+// Igualdad de objetos
+
+console.log(person)
+
+let person4 = {
+    name: 'Isidro Juan Majul',
+    alias: 'Art_Boucaya',
+    email: 'isidro@gmail.com'
+}
+
+console.log(person)
+console.log(person4)
+
+console.log(person == person4) //false
+// tener en cuenta que los objetos ya NO se guardan por VALOR, sino con una REFERENCIA (similar a un ID)
+console.log(person.name == person4.name) //true porque ya el "name" es un valor
+
+// Iteración
+
+// Se accede a las CLAVES
+for (let key in person4) {
+    // para acceder a los valores, hacerlo a través de []
+    console.log(key + ": " + person4[key])
+}
